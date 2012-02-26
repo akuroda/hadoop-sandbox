d926334f0b2eeeee18af977f449e23aacd2552c6
Akira Kuroda <akuroda@gmail.com>
2012-02-26 23:20:46 +0900

    use ChainReducer to add FinalMapper

--- src/com/akuroda/mapreduce/montecarlopi/MonteCarloPI.java
+++ src/com/akuroda/mapreduce/montecarlopi/MonteCarloPI.java
@@ -1,20 +1,25 @@
 package com.akuroda.mapreduce.montecarlopi;
 
 import java.io.IOException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.chain.ChainReducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MonteCarloPI {
 
-	static class MonteCarloPIMapper
-		extends Mapper<LongWritable, Text, Text, LongWritable> {
+	static final String IN_THE_CIRCLE = "1";
+	static final String OUT_OF_THE_CIRCLE = "0";
+
+	static class MonteCarloPIMapper extends
+			Mapper<LongWritable, Text, Text, LongWritable> {
 
 		@Override
 		protected void map(LongWritable key, Text value, Context context)
@@ -22,32 +27,59 @@ public class MonteCarloPI {
 
 			double x = Math.random() * 2 - 1;
 			double y = Math.random() * 2 - 1;
-			
-			if (Math.hypot(x, y) <= 1.0) {
-				context.write(new Text("1"), key);
-			} else {
-				context.write(new Text("0"), key);
-			}
 
+			if (Math.hypot(x, y) <= 1.0) {
+				context.write(new Text(IN_THE_CIRCLE), key);
+			} else {
+				context.write(new Text(OUT_OF_THE_CIRCLE), key);
+			}
 		}
 	}
 
-	static class MonteCarloPIReducer
-		extends Reducer<Text, LongWritable, Text, LongWritable> {
+	static class MonteCarloPIReducer extends
+			Reducer<Text, LongWritable, Text, LongWritable> {
 
 		@Override
-		protected void reduce(Text key, Iterable<LongWritable> values, Context context)
-				throws IOException, InterruptedException {
-			
+		protected void reduce(Text key, Iterable<LongWritable> values,
+				Context context) throws IOException, InterruptedException {
+
 			long counter = 0;
-			
+
 			for (LongWritable value : values) {
 				counter++;
 			}
 			context.write(key, new LongWritable(counter));
-		}		
+		}
 	}
-	
+
+	static class FinalMapper extends
+			Mapper<Text, LongWritable, Text, LongWritable> {
+
+		static Logger logger = Logger.getLogger(FinalMapper.class.getName());
+		
+		long in_points = 0;
+		long out_points = 0;
+
+		@Override
+		protected void map(Text key, LongWritable val, Context context)
+				throws IOException, InterruptedException {
+			long longval = val.get();
+			if (key.toString().equals(IN_THE_CIRCLE))
+				in_points = longval;
+			else
+				out_points = longval;
+		}
+
+		@Override
+		protected void cleanup(Context context) throws IOException,
+				InterruptedException {
+			double result = (double) in_points / (in_points + out_points) * 4.0;
+			logger.info("IN: " + in_points + ", OUT: " + out_points);
+			logger.info("result = " + result);
+			context.write(new Text("" + result), new LongWritable(in_points + out_points));
+		}
+	}
+
 	/**
 	 * @param args
 	 */
@@ -56,19 +88,27 @@ public class MonteCarloPI {
 			System.err.println("Usage: MonteCarloPI <input path> <output path>");
 			System.exit(-1);
 		}
-		
+
 		Job job = new Job();
 		job.setJarByClass(MonteCarloPI.class);
-		
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
+
 		job.setMapperClass(MonteCarloPIMapper.class);
-		job.setReducerClass(MonteCarloPIReducer.class);
-		
+
+		ChainReducer.setReducer(job, MonteCarloPIReducer.class,
+				Text.class, LongWritable.class,
+				Text.class, LongWritable.class,
+				null);
+		ChainReducer.addMapper(job, FinalMapper.class,
+				Text.class, LongWritable.class,
+				Text.class, LongWritable.class,
+				null);
+
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(LongWritable.class);
-		
+
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
